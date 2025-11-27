79f5c49163907be71dae8338e05caee3b85ef7e4
Andrew M C Wong <andrew.wong8@icloud.com>
2025-11-26 20:36:16 -0500

    ref: added ref to compare.R script

--- R/compare.R
+++ R/compare.R
@@ -6,6 +6,29 @@
 #' @param new_result List containing metrics for the newly trained signature.
 #'
 #' @return A list with comparison metrics (placeholder).
+#'
+#' @examples
+#' \dontrun{
+#' # Load pre-trained signature results
+#' pretrained_result <- esr_loadPretrainedSignature()
+#'
+#' # Train a new signature
+#' data(gse201926_trainmini)
+#' new_result <- esr_trainEndometrialSignature(
+#'   X = gse201926_trainmini$counts,
+#'   pheno = gse201926_trainmini$pheno
+#' )
+#'
+#' # Compare signatures
+#' comparison <- esr_compareSignatures(pretrained_result, new_result)
+#' comparison
+#' }
+#'
+#' @references
+#' Friedman, J., Hastie, T., & Tibshirani, R. (2010). Regularization paths for
+#' generalized linear models via coordinate descent. Journal of Statistical
+#' Software, 33(1), 1-22. <https://doi.org/10.18637/jss.v033.i01>
+#'
 #' @export
 esr_compareSignatures <- function(pretrained_result, new_result) {
   comparison <- list(delta_auc = NA_real_)
@@ -13,5 +36,3 @@ esr_compareSignatures <- function(pretrained_result, new_result) {
 }
 
 # [END]
-
-
